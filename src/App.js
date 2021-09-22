f638aac0db6fa3bb643ef1b85583198eaead1eec
guswn2521 <guswn2521@naver.com>
2021-09-22 22:54:00 +0900

    #1.3 useTabs
    // 버튼을 누를 때마다 section이 바뀜
    1. content 배열 생성 : tab과 content로 구성된 객체 2개
    2. useTabs함수 생성 :
    1) initialTab, allTabs 를 매개변수로 받아서 [currentIndex, setCurrentIndex] = useState사용
    2) allTabs가 비었거나 배열이 아닐때 함수를 바로 return -> 예외처리
    3)
    
    3. App 함수
    1) 리턴 div 안에 content.map으로 content의 각 section에 대해 button태그를 씌우고, section.tab을 보여준다.
    2) useTabs(0, content)를 변수로 생성.
    3) 버튼을 누를 때마다 useTabs의 결과로 changeItem(index)를 실행
    4) div태그안에 currentItem.content
    
    useTabs(0, content) : currentIndex에 0이 들어감. currentItem, changeItem을 리턴.
    currentItem = content[0],
    changeItem = setCurrentIndex가 됨.
    
    버튼을 클릭하면 changeItem(섹션의 index) 가 실행됨 -> setCurrentIndex(index) -> currentIndex가 누른 버튼의 index로 바뀜.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import './App.css';
+import { useState } from 'react';
+
 
 import { useInput } from './useInput';
 
+const content = [
+  {
+    tab:"Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab:"Section 2",
+    content: "I'm the content of the Section 2",
+  }
+]
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  
+  // allTabs가 비었거나 배열이아니면 return.
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  }
+}
+
 const App = () => {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Ms.", maxLen);
+  const {currentItem, changeItem} = useTabs(0, content);
 
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <div style={{width: '100px' , height: '100px', backgroundColor: 'black' }}>
-        <input placeholder="Name" type="text" {...name} />
-      </div>
-      
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }
