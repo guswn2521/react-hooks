3d5ccba8dc7f9fa12cf38adb39927f554837830d
guswn2521 <guswn2521@naver.com>
2021-10-26 00:40:38 +0900

    #2.1 useTitle 시간이 지나면 title이 바뀌는 함수 만듬. 1. useTitle함수 : initialTitle을 받아서 useState에 줌. -> setTitle을 리턴함   1) updateTitle함수 : document에서 title을 가져와서 innerText에 title을 줌.   2) useEffect(updateTitle, [title]) : title이 변할때(component willupdate) updateTitle함수를 실행함.
    2. App안에서 titleUpdater 객체에 useTitle("Loading...") 함수 넣음.
    => component didmount로 updateTitle이 실행되어 Loading이 첫 title로 들어감.
    => setTitle을 리턴함.
    
    3. setTimeout으로 titleUpdater객체에 "Home" 값을 줌.
    => setTitle("Home") 을 실행하여 component willupdate로 updateTitle을 실행, title이 몇 초뒤 Home으로 변함.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
+
+const useTitle = (initialTitle)=>{
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () =>{
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+
+
 const App = () => {
-  const sayHello = () => console.log("Hello");
-  useEffect(() => {
-    sayHello();
-  });
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Coffee"), 3000);
+  
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={() => setNumber(number+1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber+1)}>{aNumber}</button>
+     
     </div>
   );
 }
